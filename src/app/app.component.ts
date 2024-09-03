ba53c3de34d4b3a02c57a241c05e3c9c89df3559
EliandDeveloper <20222035@itla.edu.do>
2024-09-03 19:54:02 -0400

    rendering a 'Hello World' in 2 different ways and iterating an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Hello World';
+  manyWorlds: string[] = ['World', 'Universe', 'Multiverse'];
 }
