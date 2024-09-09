c9fbd406f0f06994fedfb4e032fb75ca26fc47b0
EliandDeveloper <20222035@itla.edu.do>
2024-09-09 09:25:50 -0400

    creation of the object that implements the customerModel, and validation for method callUser

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, input } from '@angular/core';
+import { Customer } from '../models/customerModel';
 import { CUSTOMER_DATA } from '../customer-data';
 
 @Component({
@@ -11,10 +11,42 @@ import { CUSTOMER_DATA } from '../customer-data';
 })
 export class CustomerComponent {
 
-  customer:any;
+  customer: Customer = {
+    customer_id: 0,
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone_number: "",
+    address: {
+        street: "",
+        city: "",
+        state: "",
+        zip_code: "",
+        country: ""
+    },
+    date_of_birth: "", 
+    registration_date: "" 
+};
 
-  callCustomer():any {
+  user: string = '';
+  userCalled: boolean = false;
+
+  callCustomer():Customer {
     const randomCustomer = Math.floor(Math.random() * CUSTOMER_DATA.length);
     return this.customer = CUSTOMER_DATA[randomCustomer];  
   }
+
+  callUser(inputValue:string){
+
+    CUSTOMER_DATA.forEach(customer => {
+        if (inputValue == customer.first_name) {
+           this.customer = customer;
+           this.user = inputValue;
+           this.userCalled = true;
+        }else{
+            this.userCalled = true;
+        }
+    });
+  }
+
 }
