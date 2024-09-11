69dfc3c8a3a8e30e0fa780613b997cf31fba55e7
EliandDeveloper <20222035@itla.edu.do>
2024-09-11 15:07:25 -0400

    getCustomer component, this component calls the data from the api and map this one to the property that implements the customersNorthwindModel

---
+++ src/app/components/get-customers/get-customers.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomersService } from '../../services/customers.service';
+import { NorthwindCustomer } from '../../models/northwindCustomer';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-get-customers',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './get-customers.component.html',
+  styleUrl: './get-customers.component.css'
+})
+export class GetCustomersComponent implements OnInit {
+
+  constructor(private customerService: CustomersService) { }
+  customersList: NorthwindCustomer[] = [];
+
+  ngOnInit(): void {
+    this.getCustomers();
+  }
+
+  getCustomers(){
+    this.customerService.getCustomers().subscribe({
+      next: (result) => {
+        this.customersList = result.data;
+        console.log(this.customersList);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+ }
+}
