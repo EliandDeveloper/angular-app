2038f72d1feb1cbf147a2626dfabb676de061a72
EliandDeveloper <20222035@itla.edu.do>
2024-09-11 15:03:29 -0400

    model of the customers entity from the northwind api

---
+++ src/app/models/northwindCustomer.ts
@@ -0,0 +1,15 @@
+export interface NorthwindCustomer {
+    customerID: string
+    companyName: string
+    contactName: string
+    contactTitle: string
+    address: string
+    city: string
+    region: any
+    postalCode: string
+    country: string
+    phone: string
+    fax: string
+    fechaRegistro: string
+  }
+  
